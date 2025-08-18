e8fe29fb91f9ecb8aa69cf498c4f7bf6f8320aa3
Ahad611 <shaikhahadsamad@gmail.com>
2025-08-19 00:12:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Ahad611/VILLAIN_MUSIC",
+    "https://github.com/Ahad611/SAFERION-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
